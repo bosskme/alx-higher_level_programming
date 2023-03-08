3a94e87f4dd50cd742890f82bc9b1be13cb5d09b
bosskme <kingsley.onele000@gmail.com>
2023-03-08 00:43:53 -0800

    Uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+def uppercase(str):
+    for char in str:
+        if 97 <= ord(char) <= 122:
+            uppercase_char = chr(ord(char) - 32)
+        else:
+            uppercase_char = char
+            print(uppercase_char, end='')
+        print()
