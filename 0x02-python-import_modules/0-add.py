147be1414cf35bcece83e2d8a510fd7ca76fe9fd
bosskme <kingsley.onele000@gmail.com>
2023-03-09 02:24:47 -0800

    Import a simple function from a simple file

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Import the add function from add_0.py
+from add_0 import add
+
+# Define a and b
+a = 1
+b = 2
+
+# Print the result of add(a, b)
+print(f"{a} + {b} = {add(a, b)}\n")
