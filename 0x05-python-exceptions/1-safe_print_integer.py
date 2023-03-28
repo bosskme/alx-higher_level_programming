b13357d1c6953dbfbb89daf66732a54b54239fd8
bosskme <kingsley.onele000@gmail.com>
2023-03-28 02:00:35 -0700

    Safe print integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-    
+    """Print an integer with "{:d}".format().
+    Args:
+        value (int): The integer to print.
+    Returns:
+        If a TypeError or ValueError occurs - False.
+        Otherwise - True.
+    """
     try:
-        # Try to format the value as an integer and print it
         print("{:d}".format(value))
-        # If no exception occurred, return True
-        return True
-    except(TypeError, ValueError):
-        # If an exception occurred, return False
-        return False
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
